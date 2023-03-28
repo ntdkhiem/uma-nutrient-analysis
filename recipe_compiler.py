9b44645b11e742c28b788992342e9b25f0a513cc
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2023-03-28 13:48:17 -0400

    Choose 3 random dates automatically instead of hard-coded

--- recipe_compiler.py
+++ recipe_compiler.py
@@ -1,39 +1,41 @@
 import json
+import random
+from datetime import datetime
 
 from bs4 import BeautifulSoup
 
 from menu_compiler import parse_name
 
-CHOSEN_LIST = [
-    "Berkshire_Dining_Commons__Friday_March_10.html",
-    "Berkshire_Dining_Commons__Friday_March_24.html",
-    "Berkshire_Dining_Commons__Monday_March_27.html",
-    "Berkshire_Dining_Commons__Sunday_March_26.html",
-    "Franklin_Dining_Commons__Sunday_March_19.html",
-    "Franklin_Dining_Commons__Friday_March_10.html",
-    "Franklin_Dining_Commons__Wednesday_March_22.html",
-    "Franklin_Dining_Commons__Monday_March_27.html",
-    "Hampshire_Dining_Commons__Tuesday_March_21.html",
-    "Hampshire_Dining_Commons__Sunday_March_12.html",
-    "Hampshire_Dining_Commons__Thursday_March_16.html",
-    "Hampshire_Dining_Commons__Monday_March_27.html",
-    "Worcester_Dining_Commons__Monday_March_20.html",
-    "Worcester_Dining_Commons__Sunday_March_19.html",
-    "Worcester_Dining_Commons__Tuesday_March_21.html",
-    "Worcester_Dining_Commons__Wednesday_March_22.html",
-]
-
-
 def format_fn(recipe_name):
     # format to valid file name
     return "".join(c for c in recipe_name if c.isalnum())
 
+def format_fnd(dining_hall, date):
+    # format date
+    date = datetime.strptime(date, '%m/%d/%Y')
+    return f"{dining_hall}__{date.strftime('%A_%B_%#d')}.html"
+
+
+def pick_dates(recipes_db):
+    """Pick 3 random dates from each dining hall that have recipes in the database"""
+    dates = []
+    for dining_hall in recipes_db.keys():
+        hall_dates = []
+        i = 0
+        while i < 3:
+            date_obj = random.choice(recipes_db[dining_hall])
+            if date_obj not in hall_dates and date_obj["meals"]:
+                hall_dates.append(format_fnd(dining_hall, date_obj["date"]))
+                i += 1
+        dates.extend(hall_dates)
+    return dates
+
 
 if __name__ == "__main__":
-    # 1) pick three random days from the four dining commons
     recipes_db = json.load(open("database.json", "r"))
     recipes_list = {}
-    for day in CHOSEN_LIST:
+    chosen_dates = pick_dates(recipes_db)
+    for day in chosen_dates:
         name, date = parse_name(day)
         if name not in recipes_list:
             recipes_list[name] = {}
@@ -43,13 +45,12 @@ if __name__ == "__main__":
                 recipes_list[name][date] = obj["meals"]
 
         soup = BeautifulSoup(open(f"html_content/{day}", "r"), "html.parser")
-        # get longmenu.aspx
-        # for each shortmenumenus in this day
+        # get longmenu.aspx for each short menu
         for menu in recipes_list[name][date].keys():
+            # -- locate the shortmenus
             el = soup.find("div", string=menu)
             # locate the link: should be its parent's next sibling
             print(el.parent.find_next_sibling("td").find("a").get("href"))
-        # -- locate the shortmenus
         # -- find the longmenu.aspx link
 
     # for dining_hall in recipes_db:
